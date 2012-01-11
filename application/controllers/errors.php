d01ffdf9099101cb5b7e66ad5b6101cfc2b09e9d
Ryan Brodkin <ryan@glabstudios.com>
2012-01-10 19:42:14 -0800

    Adding controller, styles, and images to support 404 error pages.

---
+++ application/controllers/errors.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Errors Controller for G LAB Company Portal
+ *
+ * @author Ryan Brodkin
+ * @copyright G LAB. All rights reserved.
+ * @package CodeIgniter
+ */
+
+
+class Errors extends CI_Controller
+{
+
+	function __construct () {
+		parent::__construct();
+
+		$this->template->set_layout('masthead');
+	}
+
+	/**
+	 * Displays welcome page
+	 */
+	function error_404()
+	{
+		$this->template->title('Page Not Found')->build('errors/404');
+	}
+
+}
---
+++ application/views/errors/404.php
@@ -0,0 +1,10 @@
+<div class="row">
+	<div class="span15 offset1">
+		<p>
+			We were unable to locate the page that you requested.
+		</p>
+		<a href="<?=site_url()?>" class="btn success">Back to Dashboard</a>
+		<br>
+		<br>
+	</div>
+</div>
