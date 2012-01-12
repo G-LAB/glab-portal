6b94f3cf595d086437bc25fd2fde06676ac43928
Ryan Brodkin <ryan@glabstudios.com>
2012-01-11 20:53:14 -0800

    Touched up footer styles to no longer require .legal class. Displaying current ref in footer. Displaying current branch in footer when in development environment.

--- application/views/layouts/_footer.php
+++ application/views/layouts/_footer.php
@@ -1,1 +1,9 @@
-<span class="legal">Copyright &copy; 2009-<?=date('Y')?> G LAB. &nbsp;All rights reserved.</span>
+<div class="row">
+	<div class="span6">
+		Copyright &copy; 2009-<?=date('Y')?> G LAB. &nbsp;All rights reserved.
+	</div>
+	<div class="span6 justr">
+		<?=(ENVIRONMENT == 'development') ? shell_exec('cd '.APPPATH.' && git name-rev --name-only HEAD').'/' : 'Revision'?>
+		<?=shell_exec('cd '.APPPATH.' && git rev-parse --short HEAD')?>
+	</div>
+</div>
