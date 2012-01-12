b6e15d2a2048d0b53a32d9ddb183ccda4e853039
Ryan Brodkin <ryan@glabstudios.com>
2012-01-11 20:24:08 -0800

    Adding shell dashboard controller.

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Dashboard Controller for G LAB Company Portal
+ *
+ * @author Ryan Brodkin
+ * @copyright G LAB. All rights reserved.
+ * @package CodeIgniter
+ */
+
+
+class Dashboard extends CI_Controller
+{
+	/**
+	 * Display Dashboard
+	 */
+	function index()
+	{
+		$this->template
+			->title('Dashboard')
+			->build('dashboard/main');
+	}
+
+}
