47dfa569470ed34dd10275466057ad79b0b3999f
Ryan Brodkin <ryan@glabstudios.com>
2012-01-11 21:16:21 -0800

    Added preliminary dropdown to HUD, not yet functional.

--- application/views/layouts/_hud.php
+++ application/views/layouts/_hud.php
@@ -1,1 +1,9 @@
-<a href="#" class="btn"><?=greeting()?>, <?=$this->profile->current()->name->first?>! <b class="caret"></b></a>
+<a class="btn dropdown-toggle" data-toggle="dropdown" data-target="#menu_hud">
+	<?=greeting()?>, <?=$this->profile->current()->name->first?>! <b class="caret"></b>
+</a>
+<ul id="menu_hud" class="dropdown-menu">
+	<li><a href="#">Dashboard</a></li>
+	<li><a href="#">My Profile</a></li>
+	<li class="divider"></li>
+	<li><a href="#">Logout</a></li>
+</ul>
