c8f7d4394ab485639ce800e89963550ac10bf5ea
Ryan Brodkin <ryan@glabstudios.com>
2012-01-13 06:01:45 -0800

    Fixed HUD dropdown to respond to click action.

--- application/views/layouts/_hud.php
+++ application/views/layouts/_hud.php
@@ -1,9 +1,12 @@
-<a class="btn dropdown-toggle" data-toggle="dropdown" data-target="#menu_hud">
-	<?=greeting()?>, <?=$this->profile->current()->name->first?>! <b class="caret"></b>
-</a>
-<ul id="menu_hud" class="dropdown-menu">
-	<li><a href="#">Dashboard</a></li>
-	<li><a href="#">My Profile</a></li>
-	<li class="divider"></li>
-	<li><a href="#">Logout</a></li>
-</ul>
+<div class="btn-group floatr">
+	<a class="btn dropdown-toggle" data-toggle="dropdown">
+		<?=greeting()?>, <?=$this->profile->current()->name->first?>! <b class="caret"></b>
+	</a>
+	<ul class="dropdown-menu">
+		<li><a href="#">Dashboard</a></li>
+		<li><a href="#">My Profile</a></li>
+		<li class="divider"></li>
+		<li><a href="#">Logout</a></li>
+	</ul>
+</div>
+<div class="clearfix"></div>
