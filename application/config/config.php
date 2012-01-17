d5671d783694761724d462c9ad589b6debd855da
Ryan Brodkin <ryan@glabstudios.com>
2012-01-16 20:52:37 -0800

    Adjusting config to reqire SSL cookies and utilize session encryption.

--- application/config/config.php
+++ application/config/config.php
@@ -247,7 +247,7 @@ $config['encryption_key'] = 'RrtB9RopNwqSI5lcSq4AZ2IfPbeekVsnJH7VMs6kuRGdc3U5qdX
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
-$config['sess_encrypt_cookie']	= FALSE;
+$config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
@@ -268,7 +268,7 @@ $config['sess_time_to_update']	= 300;
 $config['cookie_prefix']	= "";
 $config['cookie_domain']	= "";
 $config['cookie_path']		= "/";
-$config['cookie_secure']	= FALSE;
+$config['cookie_secure']	= TRUE;
 
 /*
 |--------------------------------------------------------------------------
