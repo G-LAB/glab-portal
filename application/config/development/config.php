7bf4132c617c0be6a647865e6eeecd571d611e92
Ryan Brodkin <ryan@glabstudios.com>
2012-03-04 22:30:12 -0800

    Default log level is debug during development.

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -14,6 +14,28 @@ require_once APPPATH.'config/config.php';
 */
 $config['index_page'] = 'index.php';
 
+/*
+|--------------------------------------------------------------------------
+| Error Logging Threshold
+|--------------------------------------------------------------------------
+|
+| If you have enabled error logging, you can set an error threshold to
+| determine what gets logged. Threshold options are:
+| You can enable error logging by setting a threshold over zero. The
+| threshold determines what gets logged. Threshold options are:
+|
+|	0 = Disables logging, Error logging TURNED OFF
+|	1 = Error Messages (including PHP errors)
+|	2 = Debug Messages
+|	3 = Informational Messages
+|	4 = All Messages
+|
+| For a live site you'll usually only enable Errors (1) to be logged otherwise
+| your log files will fill up very fast.
+|
+*/
+$config['log_threshold'] = 2;
+
 
 /* End of file config.php */
 /* Location: ./application/config/config.php */
