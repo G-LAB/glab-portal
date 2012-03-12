441a7ad66ad1d524dda6c3ec2747ba178cdeaf50
Ryan Brodkin <ryan@glabstudios.com>
2012-03-11 20:52:47 -0700

    Changing session timeout to 15 minutes.

--- application/config/config.php
+++ application/config/config.php
@@ -245,14 +245,14 @@ $config['encryption_key'] = 'RrtB9RopNwqSI5lcSq4AZ2IfPbeekVsnJH7VMs6kuRGdc3U5qdX
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 600;
+$config['sess_expiration']		= 900;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 1;
+$config['sess_time_to_update']	= 120;
 
 /*
 |--------------------------------------------------------------------------
